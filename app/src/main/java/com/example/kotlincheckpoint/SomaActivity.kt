2258507c1ac742cdee3f77dd894ec54bb4a3da36
odsvictor <odsvictor2001@gmail.com>
2022-04-18 16:28:42 -0300

    Telas de adição e subtração funcionando, sem lista de historico e sem menu principal!

---
+++ app/src/main/java/com/example/kotlincheckpoint/SomaActivity.kt
@@ -0,0 +1,22 @@
+package com.example.kotlincheckpoint
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SomaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soma)
+
+        val etN1: TextView = findViewById(R.id.etN1)
+        val etN2: TextView = findViewById(R.id.etN2)
+        val btIgual: TextView = findViewById(R.id.btIgual)
+        val tvResultado: TextView = findViewById(R.id.tvResultado)
+
+        btIgual.setOnClickListener {
+            tvResultado.text = "${etN1.text.toString()} + ${etN2.text.toString()} = "+(etN1.text.toString().toDouble()+etN2.text.toString().toDouble()).toString()
+        }
+
+    }
+}
