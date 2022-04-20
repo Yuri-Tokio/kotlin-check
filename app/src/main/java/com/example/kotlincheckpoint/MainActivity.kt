77b13c51015250f4a13ca71b300d128aed8be0a2
Yuri-Tokio <yuri@sheild.com.br>
2022-04-20 03:52:40 -0300

    Yuri e Victor OK
    falta mergear task joao

--- app/src/main/java/com/example/kotlincheckpoint/MainActivity.kt
+++ app/src/main/java/com/example/kotlincheckpoint/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.kotlincheckpoint
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.example.kotlincheckpoint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btaSoma.setOnClickListener { irSomar() }
+
+        binding.btaSubtracao.setOnClickListener { irSubtrair() }
+
+    }
+
+    private fun irSomar(){
+        val intent = Intent(this, SomaActivity::class.java)
+
+        startActivity(intent)
+    }
+
+    private fun irSubtrair(){
+        val intent = Intent(this, SubtracaoActivity::class.java)
+
+        startActivity(intent)
     }
 }
