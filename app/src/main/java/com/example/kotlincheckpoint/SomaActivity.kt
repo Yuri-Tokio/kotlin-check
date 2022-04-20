b361fd11e52a9e9406323587f5b8bc8c42498292
Yuri-Tokio <yuri@sheild.com.br>
2022-04-20 20:55:41 -0300

    falta databaseSoma

--- app/src/main/java/com/example/kotlincheckpoint/SomaActivity.kt
+++ app/src/main/java/com/example/kotlincheckpoint/SomaActivity.kt
@@ -1,22 +1,33 @@
 package com.example.kotlincheckpoint
 
-import android.os.Bundle
 import android.widget.TextView
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.provider.ContactsContract
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlincheckpoint.adapter.ItemAdapter
 class SomaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_soma)
 
-        val etN1: TextView = findViewById(R.id.etN1)
-        val etN2: TextView = findViewById(R.id.etN2)
-        val btIgual: TextView = findViewById(R.id.btIgual)
-        val tvResultado: TextView = findViewById(R.id.tvResultado)
+        val etN1: TextView = findViewById(R.id.etN1Som)
+        val etN2: TextView = findViewById(R.id.etN2Som)
+        val btIgual: TextView = findViewById(R.id.btIgualSom)
+        val tvResultado: TextView = findViewById(R.id.tvResultadoSom)
 
         btIgual.setOnClickListener {
             tvResultado.text = "${etN1.text.toString()} + ${etN2.text.toString()} = "+(etN1.text.toString().toDouble()+etN2.text.toString().toDouble()).toString()
         }
 
+        val rvListaSomas:RecyclerView = findViewById(R.id.rvListaSomas)
+        rvListaSomas.layoutManager = LinearLayoutManager(this)
+        rvListaSomas.adapter = ItemAdapter(DatabaseSomas.listaSomas)
+    }
+
+    fun registrarConta(valor1:String, valor2:String, resultado:String){
+        DatabaseSomas.listaSomas.add(Operacao(valor1 = valor1.toDouble(), valor2 = valor2.toDouble(), resultado = resultado.toDouble()))
     }
 }
--- app/src/main/java/com/example/kotlincheckpoint/SubtracaoActivity.kt
+++ app/src/main/java/com/example/kotlincheckpoint/SubtracaoActivity.kt
@@ -3,20 +3,31 @@ package com.example.kotlincheckpoint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlincheckpoint.adapter.ItemAdapter
 
 class SubtracaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_subtracao)
 
-        val etN1: TextView = findViewById(R.id.etN1)
-        val etN2: TextView = findViewById(R.id.etN2)
-        val btIgual: TextView = findViewById(R.id.btIgual)
-        val tvResultado: TextView = findViewById(R.id.tvResultado)
+        val etN1: TextView = findViewById(R.id.etN1Sub)
+        val etN2: TextView = findViewById(R.id.etN2Sub)
+        val btIgual: TextView = findViewById(R.id.btIgualSub)
+        val tvResultado: TextView = findViewById(R.id.tvResultadoSub)
 
         btIgual.setOnClickListener {
             tvResultado.text = "${etN1.text.toString()} - ${etN2.text.toString()} = "+(etN1.text.toString().toDouble()-etN2.text.toString().toDouble()).toString()
+            registrarSubtracao(etN1.text.toString(),etN2.text.toString(),tvResultado.text.toString())
         }
 
+        val rvListaSubtracoes: RecyclerView = findViewById(R.id.rvListaSubtracoes)
+        rvListaSubtracoes.layoutManager = LinearLayoutManager(this)
+        rvListaSubtracoes.adapter = ItemAdapter(DatabaseSubtracoes.listaSubtracoes)
+    }
+
+    fun registrarSubtracao(valor1:String, valor2:String, resultado:String){
+        DatabaseSubtracoes.listaSubtracoes.add(Operacao(valor1 = valor1.toDouble(), valor2 = valor2.toDouble(), resultado = resultado.toDouble()))
     }
 }
